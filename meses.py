42d4d7c517898a5d62989497c867aa38ba9f5e0c
Jose matheus <j.matheus.v.souza@gmail.com>
2025-04-07 10:26:27 -0300

    meses

---
+++ meses.py
@@ -0,0 +1,28 @@
+mes = int (input(f"escreva o numero do mes (exe: 1)"))
+
+if mes == 1:
+    print("janeiro")
+if mes == 2:
+    print("fevereiro")
+if mes == 3:
+    print("março")
+if mes == 4:
+    print("abril")
+if mes == 5:
+    print("maio")
+if mes == 6:
+    print("junho")
+if mes == 7:
+    print("julho")
+if mes == 8:
+    print("agosto")
+if mes == 9:
+    print("setembro")
+if mes == 10:
+    print("outubro")
+if mes == 11:
+    print("novembro")
+elif mes <=13:
+    print("erro")
+else :
+    print("dezembro")
